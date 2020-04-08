870750f0e13545b589dcaa47ac08c6ad6d3a76c5
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-08 01:21:53 -0400

    saving before I break it

--- 04-web-APIs/09-Stu_SpeedReader/Unsolved/script.js
+++ 04-web-APIs/09-Stu_SpeedReader/Unsolved/script.js
@@ -1,5 +1,10 @@
 // used .split to create an array of strings so I did not have to individually make each word a string by itself.
-var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.".split(" ")
+var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice. End.".split(" ")
+var pTag = document.querySelector("p")
+var h1Tag = document.querySelector("h1")
+var h2Tag = document.querySelector("h2")
+var viewPort = document.querySelector(".viewport")
+
 
 // created poemText because I need a way to access the paragraph later
 var poemText = document.querySelector("p")
@@ -7,8 +12,15 @@ var poemText = document.querySelector("p")
 // created count to iterate over the array
 var count = 0
 
+h1Tag.setAttribute("style", "font-size: 50px;")
+h2Tag.setAttribute("style", "font-size: 50px;")
+pTag.setAttribute("style", "font-size: 50px; color: #c25ba6;")
+viewPort.setAttribute("style", "background-color: #ede8ec; text-align: center;")
+
+
+
 function prepareRead() {
-  setTimeout(speedRead, 5000)
+  setTimeout(speedRead, 3000)
 }
 
 prepareRead()
@@ -17,5 +29,12 @@ prepareRead()
 function speedRead() {
   poemText.innerHTML = poem[count]
   count++
-  setTimeout(speedRead, 500)
+
+  // added if statement and poem.length to make the poem end on the last word instead of "undefined"
+  if (count < poem.length) {
+    setTimeout(speedRead, 500)
+  }
+
+
 }
+
