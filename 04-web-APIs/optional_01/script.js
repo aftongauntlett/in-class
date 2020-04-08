b02694dc712f30992ad3e6bf43505b0390cf6007
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-08 00:50:40 -0400

    finished optional

--- 04-web-APIs/optional_01/script.js
+++ 04-web-APIs/optional_01/script.js
@@ -1,52 +1,35 @@
+// chose .viewport because it was a class
+var viewPort = document.querySelector(".viewport")
+var mainTag = document.querySelector("main")
+var hTag = document.querySelector("h1")
 
-function viewportColor () {
-console.log("meow");
+// used all because there are 2 paragraphs
+var pTags = document.querySelectorAll("p")
+
+// added [1] becaues I need to be able to select the second element in the array
+var sectionTwo = document.querySelectorAll("section")[1]
+
+// used all because there is more than one <a> tag in the body
+var aTag = document.querySelectorAll("a")
+var imgTag = document.querySelector("img")
+
+// css styling
+viewPort.setAttribute("style", "background-color: #eeeddd;")
+mainTag.setAttribute("style", "padding: 50px;")
+sectionTwo.setAttribute("style", "border: 1px solid red; padding: 8px; text-align: center;")
+hTag.setAttribute("style", "text-align: center;")
+
+// used [0] and [1] to change the correct elements in the array. Chose href because we are working with a link.
+aTag[0].setAttribute("href", "https://www.google.com/")
+aTag[0].setAttribute("target", "_blank")
+aTag[1].setAttribute("href", "https://www.nytimes.com/")
+aTag[1].setAttribute("target", "_blank")
+
+// chose src because we are working with an image
+imgTag.setAttribute("src", "https://wallpaperaccess.com/full/439031.jpg")
+imgTag.setAttribute("style", "text-align: center; height: auto; width: 100%;")
+
+// created a for loop to set the same styling to all paragraphs in the body
+for (var i = 0; i < pTags.length; i++) {
+  pTags[i].setAttribute("style", "font-size: 14px; color: #282828; margin-bottom: 12px;")
 }
-viewportColor ()
-  
-var h1 = document.querySelector("h1");
-h1.style.color = "pink";
-
-var body = document.querySelector("body");
-varisBlue - false;
-setInterval(function() {
-    if(isBlue) {
-        body.style.background = "#3498db";
-    }
-    isBlue = !blue;
-}, 1000);
-
-
-  
-  //  Instructions:
-
-  //    1. Take a look at the page above and study the DOM elements. Then do the following:
-  //       a. Give the div w/ a class of viewport a background color of #eeeddd
-  //       b. Put a padding of 50px on the main tag
-  //       c. ALL p tags should have these styles:
-  //           font-size: 14px;
-  //           color: #282828;
-  //           margin-bottom: 12px;
-  //       d. The SECOND section should have this styling:
-  //           border: 1px solid red;
-  //           padding: 8px;
-  //           text-align: center;
-  //       e. The first link tag should go to Google, in a separate tab
-  //       f. The second link tag should go to NYTimes.com, in a separate tab
-  //       g. Create a NEW image tag, have the src attribute point to an image file somewhere
-  //          on the web, and then insert that image as the last element in the body tag.
-  //       h. Do any other styling that you want.
-  //       IF YOU WANT, POST A SCREENSHOT OF YOUR FINAL PAGE IN RANDOM.
-  //       BONUS:
-  //       a. When the user clicks on the second link, make the image you created above disappear.
- 
-
-// document.getElementById() //it shows a javascript object of what we selected
-// document.getElementsByClassName() //shows a javascript object for all the html items with the same class
-// document.getElementsByTagName() //shows a javascript object for all the html items with the same tags
-// document.querySelector()  //takes a css style selector and allows you to use this to select an element as an object
-// EXAMPLES:
-// document.getElementById("highlight");
-// document.getElementsByClassName("bolded");
-// document.getElementsByTagName("li");
-// document.querySelector("#highlight");
